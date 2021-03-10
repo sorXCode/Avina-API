6d008d0f95dcb31e6df0c9e6c65c125e636668a0
sorxcode <sorxcode@gmail.com>
2021-03-10 01:00:24 +0100

    updated categories permission

--- categories/urls.py
+++ categories/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CategoryList, CategoryDetail, CategoryCreate
+from .views import CategoryList, CategoryDetail
 
 urlpatterns = [
-    path('', CategoryList.as_view()),
-    path('', CategoryCreate.as_view()),
-    path('<int:pk>/', CategoryDetail.as_view()),
+    path('', CategoryList.as_view({'get': 'list', 'post': 'create'})),
+    path('<int:pk>/', CategoryDetail.as_view({'patch': 'partial_update',
+                                    'delete': 'destroy',
+                                    'get': 'retrieve'
+                                    })),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- categories/views.py
+++ categories/views.py
@@ -2,20 +2,30 @@ from rest_framework import generics
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
 
+class PermissionMixin:
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
 
-class CategoryList(generics.ListAPIView):
+class CategoryList(PermissionMixin, ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes_by_action = {'create': [IsAdminUser],
+                                    'list': [IsAuthenticated]}
 
-class CategoryCreate(generics.CreateAPIView):
+
+
+class CategoryDetail(PermissionMixin, ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAdminUser, ]
-
-
-class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    permission_classes = [IsAdminUser, ]
+    permission_classes_by_action = {'partial_update': [IsAdminUser],
+                                    'destroy': [IsAdminUser],
+                                    'retrieve': [IsAuthenticated]
+                                    }
